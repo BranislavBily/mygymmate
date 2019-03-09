ea3c77ec65e4b381646253c473d811c251d7ed21
Sebastian Hresko <hresko@spse-po.sk>
2019-03-09 17:35:33 +0100

    SMALL nic len tak

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -10,7 +10,7 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("FXML/UserHomeScreen.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("FXML/login.fxml"));
         primaryStage.setTitle("Hello Brano");
         primaryStage.setScene(new Scene(root));
         primaryStage.show();
