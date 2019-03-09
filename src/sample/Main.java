0c49b3a695794c66419e3ec029ac20359aec7122
Sebastian Hresko <hresko@spse-po.sk>
2019-03-09 19:17:01 +0100

    SMALL login design

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -11,7 +11,7 @@ public class Main extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("FXML/login.fxml"));
-        primaryStage.setTitle("Hello Brano");
+        primaryStage.setTitle("MyGymMate");
         primaryStage.setScene(new Scene(root));
         primaryStage.show();
     }
