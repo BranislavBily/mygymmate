20b890c68a3228599eb9085afa655a45f609c608
Branislav Bily <Branislav Bily>
2019-03-30 19:00:06 +0100

    MERGE Result

--- src/sample/Controllers/SceneControllers/Trainee/TraineeHomeScreen.java
+++ src/sample/Controllers/SceneControllers/Trainee/TraineeHomeScreen.java
@@ -2,11 +2,18 @@ package sample.Controllers.SceneControllers.Trainee;
 
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import sample.Controllers.Controller;
+
+import sample.Modules.ModuleFXML;
+import sample.Modules.ModuleTitles;
+import sample.Users.Trainee.Trainee;
 
 
 public class TraineeHomeScreen extends Controller {
 
+
+    private Trainee trainee;
     @FXML
     private Button buttonLogOut;
     @FXML
@@ -16,7 +23,35 @@ public class TraineeHomeScreen extends Controller {
     @FXML
     private Button buttonMeasure;
 
+    @FXML
+    private Label labelUsername;
+
+    public Label getLabelUsername() {
+        return labelUsername;
+    }
+
+    public void setLabelUsername(Label labelUsername) {
+        this.labelUsername = labelUsername;
+    }
+
+    public Trainee getTrainee() {
+        return trainee;
+    }
+
+    public void setTrainee(Trainee trainee) {
+        this.trainee = trainee;
+    }
+
+    @FXML
    public void onButtonLogOutPressed() {
         super.onButtonLogOutPressed(buttonLogOut);
     }
+
+@FXML
+    private void onButtonWorkoutPressed(){ setScene(buttonWorkout.getScene(),ModuleFXML.WORKOUTS_HOME_SCREEN,ModuleTitles.USER_HOME_SCREEN);}
+
+
+@FXML private void onButtonMeasurePressed(){setScene(buttonMeasure.getScene(),ModuleFXML.MEASURE_HOME_SCREEN,ModuleTitles.USER_HOME_SCREEN);}
+
 }
+
