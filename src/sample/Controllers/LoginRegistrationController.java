9ad534b098bc222f32128469c6b5971963aeed28
Branislav Bily <branislav.bily@gmail.com>
2019-04-09 16:45:56 +0200

    REF Added Methods for changing to Login and Register to LoginRegistrationController

--- src/sample/Controllers/LoginRegistrationController.java
+++ src/sample/Controllers/LoginRegistrationController.java
@@ -1,14 +1,20 @@
 package sample.Controllers;
 
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.ChoiceBox;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 import javafx.scene.effect.DropShadow;
 import javafx.scene.paint.Color;
+import javafx.stage.Stage;
+import sample.Modules.ModuleFXML;
+import sample.Modules.ModuleTitles;
+
+import java.io.IOException;
 
 public class LoginRegistrationController extends Controller {
-
-    //Treba zmenit aj focus
 
     protected void displayErrorFeedbackUsername(TextField textField){
         textField.setText("");
@@ -32,10 +38,29 @@ public class LoginRegistrationController extends Controller {
         choiceBox.setEffect(dropShadow);
     }
 
-
-
-    //Reverse vsetok feedback aby to vyzeralo tak ako pred zobrazenim feedbacku
-    protected void reverseFeedback() {
-
+    protected void setSceneToLogin(Scene scene) {
+        Stage stage = (Stage) scene.getWindow();
+        changeScene(stage, ModuleFXML.LOGIN, ModuleTitles.LOG_IN);
     }
+
+    protected void setSceneToRegister(Scene scene) {
+        Stage stage = (Stage) scene.getWindow();
+        changeScene(stage, ModuleFXML.REGISTER, ModuleTitles.REGISTER);
+    }
+
+    //This method is used when sending User is not necessary
+    private void changeScene(Stage stage, String fxml, String title) {
+        FXMLLoader loader = new FXMLLoader(getClass().getResource(fxml));
+        try {
+            Parent root = loader.load();
+            Scene scene = new Scene(root);
+            stage.setScene(scene);
+            stage.setTitle(title);
+            stage.setResizable(false);
+            stage.show();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
 }
