55b9f0c3b9bc6ca8351e86e74886287cbfc789f9
Branislav Bily <branislav.bily@gmail.com>
2019-04-09 21:08:09 +0200

    SMALL Added admin home screen and changed User Home Screen title

--- src/sample/Modules/ModuleTitles.java
+++ src/sample/Modules/ModuleTitles.java
@@ -3,8 +3,9 @@ package sample.Modules;
 public interface ModuleTitles {
     String LOG_IN = "Log in";
     String REGISTER = "Sign Up";
-    String REGISTER_INFO = "Register Info";
-    String USER_HOME_SCREEN = "MyGymMate";
+    String REGISTER_INFO = "Register Info: ";
+    String USER_HOME_SCENE = "MyGymMate welcomes ";
+    String ADMIN_HOME_SCENE = "Hi admin!";
     String WORKOUTS = "Workouts";
     String MEASURE = "Measure";
     String DIET = "Diet";
