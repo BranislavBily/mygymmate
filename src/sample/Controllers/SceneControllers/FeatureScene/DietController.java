92587d32e45d67210597c613ebb6221d896825e0
Branislav Bily <branislav.bily@gmail.com>
2019-04-11 10:23:06 +0200

    REF Pressed ctlr alt l and added scene flow

--- src/sample/Controllers/SceneControllers/FeatureScene/DietController.java
+++ src/sample/Controllers/SceneControllers/FeatureScene/DietController.java
@@ -4,30 +4,49 @@ import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import sample.Controllers.Controller;
+import sample.Controllers.HomeSceneController;
 import sample.Modules.ModuleTitles;
+import sample.Users.Trainee.Trainee;
+import sample.Users.Trainer.Trainer;
+import sample.Users.User;
 
-public class DietController extends Controller {
+public class DietController extends HomeSceneController {
 
+    private User user;
+    private Trainee trainee;
+    private Trainer trainer;
+    private boolean isTraineeDiet;
 
-
-
-@FXML
+    @FXML
     private Button buttonDiets;
-@FXML
+    @FXML
     private Button buttonWieight;
-@FXML
+    @FXML
     private Button buttonBMI;
-@FXML
+    @FXML
     private Label usernameLabel;
 
 
+    @FXML
+    private void onGoBackImagePressed() {
+        if(isTraineeDiet) {
+            setSceneToTraineeHomeScene(buttonBMI.getScene(), user);
+        } else {
+            setSceneToTrainerHomeScene(buttonBMI.getScene(), user);
+        }
+    }
 
-
-@FXML
-private void onGoBackImagePressed(){
-    setScene(buttonDiets.getScene(), "../../FXML/traineeHomeScene.fxml", ModuleTitles.USER_HOME_SCENE);
-
-
-}
+    public void setUser(User user) {
+        this.user = user;
+        if(user instanceof Trainee) {
+            isTraineeDiet = true;
+            // while loadTrainee does not load from db
+            //trainee = loadTrainee(user.getId());
+        } else {
+            isTraineeDiet = false;
+            //while loadTrainer does not load from db
+            //trainer = loadTrainer(user.getId());
+        }
+    }
 
 }
