a1046c6c77db003793f9f17bfa3a2557a5764623
Branislav Bily <branislav.bily@gmail.com>
2019-04-29 20:07:39 +0200

    SMALL Changed titles for trainer and trainee

--- src/sample/Modules/ModuleTitles.java
+++ src/sample/Modules/ModuleTitles.java
@@ -4,8 +4,8 @@ public interface ModuleTitles {
     String LOG_IN = "Log in";
     String REGISTER = "Sign Up";
     String REGISTER_INFO = "Register Info: ";
-    String TRAINEE_HOME_SCENE = "MyGymMate welcomes ";
-    String TRAINER_HOME_SCENE = "MyGymMate welcomes ";
+    String TRAINEE_HOME_SCENE = "Hi Trainee!";
+    String TRAINER_HOME_SCENE = "Hi Trainer!";
     String ADMIN_HOME_SCENE = "Hi admin!";
     String WORKOUTS = "Workouts";
     String MEASURE = "Measure";
