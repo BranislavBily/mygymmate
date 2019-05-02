ad15cda064171e123587cbb96a0209543457eaef
Branislav Bily <branislav.bily@gmail.com>
2019-05-02 17:30:36 +0200

    FEAT Password dialog for confirming password

--- src/sample/Dialogs/PasswordConfirmation.java
+++ src/sample/Dialogs/PasswordConfirmation.java
@@ -1,4 +1,49 @@
 package sample.Dialogs;
 
-public class PasswordConfirmation {
+import javafx.application.Platform;
+import javafx.geometry.Insets;
+import javafx.scene.control.ButtonBar;
+import javafx.scene.control.ButtonType;
+import javafx.scene.control.Dialog;
+import javafx.scene.control.PasswordField;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.Priority;
+
+public class PasswordConfirmation extends Dialog<String> {
+    private PasswordField passwordField;
+
+    public PasswordConfirmation(String titleText, String headerText, String contextText) {
+        setTitle(titleText);
+        setHeaderText(headerText);
+        setContentText(contextText);
+
+        ButtonType passwordButtonType = new ButtonType("Confirm", ButtonBar.ButtonData.OK_DONE);
+        getDialogPane().getButtonTypes().addAll(passwordButtonType, ButtonType.CANCEL);
+
+        passwordField = new PasswordField();
+        passwordField.setPromptText("Password");
+
+        HBox hBox = new HBox();
+        hBox.getChildren().add(passwordField);
+        hBox.setPadding(new Insets(20));
+
+        HBox.setHgrow(passwordField, Priority.ALWAYS);
+
+        getDialogPane().setContent(hBox);
+
+        Platform.runLater(() -> passwordField.requestFocus());
+
+        setResultConverter(dialogButton -> {
+            if (dialogButton == passwordButtonType) {
+                return passwordField.getText();
+            }
+            return null;
+        });
+    }
+
+    public PasswordField getPasswordField() {
+        return passwordField;
+    }
 }
+
+//This PasswordDialog was created by https://github.com/drguildo and full credits go to him
