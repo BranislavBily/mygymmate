2c140ec092008edac9b7bc55f1aa2c580ab43131
Branislav Bily <branislav.bily@gmail.com>
2019-05-02 17:30:57 +0200

    FEAT Dialog when user inputs wrong password

--- src/sample/Dialogs/WrongPasswordDialog.java
+++ src/sample/Dialogs/WrongPasswordDialog.java
@@ -1,4 +1,14 @@
 package sample.Dialogs;
 
-public class WrongPasswordDialog {
+import javafx.scene.control.Alert;
+
+public class WrongPasswordDialog extends Alert {
+
+    public WrongPasswordDialog(AlertType alertType) {
+        super(alertType);
+        super.setTitle("Wrong password");
+        super.setHeaderText("No action will be done!");
+        super.showAndWait();
+    }
+
 }
