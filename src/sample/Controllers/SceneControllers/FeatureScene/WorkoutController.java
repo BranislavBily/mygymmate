dd3a35337cd9f6f3b5d3f33fcb9bac4be4d9bb25
Branislav Bily <branislav.bily@gmail.com>
2019-05-03 17:11:46 +0200

    FEAT Added fragments functionality, moved Table into WorkoutsFragmentController

--- src/sample/Controllers/SceneControllers/FeatureScene/WorkoutController.java
+++ src/sample/Controllers/SceneControllers/FeatureScene/WorkoutController.java
@@ -1,51 +1,91 @@
 package sample.Controllers.SceneControllers.FeatureScene;
 
-import db.DTO.Workout;
 import db.DatabaseModuleUser;
-import db.DatabaseModuleWorkout;
-import javafx.collections.FXCollections;
-import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
-import javafx.scene.control.TableColumn;
-import javafx.scene.control.TableView;
-import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.layout.AnchorPane;
+import sample.Controllers.FragmentControllers.WorkoutSceneFragments.AddWorkoutFragmentController;
+import sample.Controllers.FragmentControllers.WorkoutSceneFragments.ProgressWorkoutController;
+import sample.Controllers.FragmentControllers.WorkoutSceneFragments.WorkoutsFragmentController;
 import sample.Controllers.SceneControllers.HomeSceneController;
+import sample.Resources.ResourceFXML;
 import sample.Resources.ResourceUserType;
 import sample.Session;
 
-import java.util.ArrayList;
+import java.io.IOException;
 
 public class WorkoutController extends HomeSceneController {
     private int userID;
+
+    @FXML
+    private AnchorPane mainFragment;
     @FXML
     private Button buttonWorkouts;
+    @FXML
+    private Button buttonAddWorkout;
     @FXML
     private Button buttonProgress;
     @FXML
     private Label labelUsername;
-    @FXML
-    private TableView<Workout> tableViewWorkouts;
-    @FXML
-    private TableColumn<Workout, String> tableColumnExercise;
-    @FXML
-    private TableColumn<Workout, String> tableColumnRepetitions;
-    @FXML
-    private TableColumn<Workout, String> tableColumnWeight;
-    @FXML
-    private TableColumn<Workout, String> tableColumnDate;
 
     public void onCreate() {
         userID = Session.getUserID();
         setLabel();
-        loadWorkoutsIntoTable();
+        onButtonWorkoutsPressed();
     }
 
     private void setLabel() {
         DatabaseModuleUser databaseModuleUser = new DatabaseModuleUser();
         String username = databaseModuleUser.getUsername(userID);
         setLabelUsername(labelUsername, username);
+    }
+
+    @FXML
+    private void onButtonWorkoutsPressed() {
+        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource(ResourceFXML.WORKOUTS_FRAGMENT));
+        try {
+            Parent fragment = fxmlLoader.load();
+            WorkoutsFragmentController workoutsFragmentController = fxmlLoader.getController();
+            workoutsFragmentController.onCreate();
+            mainFragment.getChildren().setAll(fragment);
+            removeButtonActiveEffect(buttonAddWorkout ,buttonProgress, buttonWorkouts);
+            buttonWorkouts.getStyleClass().add("buttonActive");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @FXML
+    private void onButtonAddWorkoutPressed() {
+        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource(ResourceFXML.ADD_WORKOUT_FRAGMENT));
+        try {
+            Parent fragment = fxmlLoader.load();
+            AddWorkoutFragmentController addWorkoutFragmentController = fxmlLoader.getController();
+            addWorkoutFragmentController.onCreate();
+            mainFragment.getChildren().setAll(fragment);
+            removeButtonActiveEffect(buttonAddWorkout ,buttonProgress, buttonWorkouts);
+            buttonAddWorkout.getStyleClass().add("buttonActive");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @FXML
+    private void onButtonProgressPressed() {
+        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource(ResourceFXML.PROGRESS_WORKOUT_FRAGMENT));
+        try {
+            Parent fragment = fxmlLoader.load();
+            ProgressWorkoutController addWorkoutFragmentController = fxmlLoader.getController();
+            addWorkoutFragmentController.onCreate();
+            mainFragment.getChildren().setAll(fragment);
+            removeButtonActiveEffect(buttonAddWorkout ,buttonProgress, buttonWorkouts);
+            buttonProgress.getStyleClass().add("buttonActive");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
     @FXML
@@ -57,16 +97,5 @@ public class WorkoutController extends HomeSceneController {
         } else {
             setSceneToTrainerHomeScene(buttonWorkouts.getScene());
         }
-    }
-
-    private void loadWorkoutsIntoTable() {
-        DatabaseModuleWorkout databaseModuleWorkout = new DatabaseModuleWorkout();
-        ArrayList<Workout> workouts = databaseModuleWorkout.loadWorkouts(Session.getUserID());
-        tableColumnExercise.setCellValueFactory(new PropertyValueFactory("Exercise"));
-        tableColumnRepetitions.setCellValueFactory(new PropertyValueFactory("Repetitions"));
-        tableColumnWeight.setCellValueFactory(new PropertyValueFactory("Weight"));
-        tableColumnDate.setCellValueFactory(new PropertyValueFactory("Date"));
-        ObservableList<Workout> workoutObservableList = FXCollections.observableList(workouts);
-        tableViewWorkouts.setItems(workoutObservableList);
     }
 }
