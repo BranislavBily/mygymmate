2e9ddb6914683c6bd7a12a7b873d0c3b73559daa
Sebastian Hresko <hreskosebastian@gmail.com>
2019-05-05 12:03:37 +0200

    dietFragment all UI elements on place without any functionality

--- src/sample/Controllers/FragmentControllers/DietSceneFragments/DietFragmentController.java
+++ src/sample/Controllers/FragmentControllers/DietSceneFragments/DietFragmentController.java
@@ -1,8 +1,49 @@
 package sample.Controllers.FragmentControllers.DietSceneFragments;
 
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.ProgressBar;
+import javafx.scene.control.TextField;
+
 public class DietFragmentController {
 
+    @FXML
+    private ProgressBar caloriesProgressBar;
+    @FXML
+    private ProgressBar proteinProgressBar;
+    @FXML
+    private ProgressBar waterProgressBar;
+    @FXML
+    private TextField caloriesTextField;
+    @FXML
+    private TextField proteinTextField;
+    @FXML
+    private TextField waterTextField;
+    @FXML
+    private Button buttonSaveCalories;
+    @FXML
+    private Button buttonSaveProtein;
+    @FXML
+    private Button buttonSaveWater;
+
+
     public void onCreate() {
+
+    }
+
+@FXML
+    private void onButtonSaveCaloriesPressed(){
+
+    }
+@FXML
+    private void onButtonSaveProteinPressed(){
+
+
+    }
+@FXML
+    private void onButtonSaveWaterPressed(){
+
 
     }
 
