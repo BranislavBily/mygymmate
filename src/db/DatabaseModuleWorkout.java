5b6ff994aea363678db4caa8c8db658bc7b65b46
Branislav Bily <branislav.bily@gmail.com>
2019-05-05 17:08:03 +0200

    FEAT Added method for getting all workouts of exercise, a bit of refactoring

--- src/db/DatabaseModuleWorkout.java
+++ src/db/DatabaseModuleWorkout.java
@@ -9,26 +9,28 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
+import java.util.LinkedHashSet;
 
 public class DatabaseModuleWorkout {
 
     private Connection connection;
+    private int userID;
 
     public DatabaseModuleWorkout() {
         connection = SqliteConnection.connector();
-        if(connection == null) System.exit(1);
+        userID = Session.getUserID();
+        if (connection == null) System.exit(1);
     }
 
-    public ArrayList<Workout> loadWorkouts(int userID) {
+    public ArrayList<Workout> loadWorkouts() {
         ArrayList<Workout> workouts = new ArrayList<>();
-        ResultSet resultSet;
         String query = "select * from " + ResourceTables.WORKOUTS + " where userID = ?";
-        try(PreparedStatement preparedStatement = connection.prepareStatement(query)) {
+        try (PreparedStatement preparedStatement = connection.prepareStatement(query)) {
             preparedStatement.setInt(1, userID);
-            resultSet = preparedStatement.executeQuery();
-            while(resultSet.next()) {
+            ResultSet resultSet = preparedStatement.executeQuery();
+            while (resultSet.next()) {
                 Workout workout = loadWorkoutFromResultSet(resultSet);
-                if(workout != null) workouts.add(workout);
+                if (workout != null) workouts.add(workout);
             }
             return workouts;
         } catch (SQLException e) {
@@ -37,13 +39,18 @@ public class DatabaseModuleWorkout {
         }
     }
 
+    /**
+     * Returns Workout from ResultSet
+     * @param resultSet from which workout will be loaded
+     * @return Workout from ResultSet
+     */
     private Workout loadWorkoutFromResultSet(ResultSet resultSet) {
         Workout workout = new Workout();
         try {
             workout.setExercise(resultSet.getString("exercise"));
             workout.setRepetitions(resultSet.getInt("repetitions"));
             String weight = Double.toString(resultSet.getDouble("weight"));
-            if(weight.equals("0.0")) {
+            if (weight.equals("0.0")) {
                 weight = "Bodyweight";
             }
             workout.setWeight(weight);
@@ -55,15 +62,39 @@ public class DatabaseModuleWorkout {
         }
     }
 
-    public boolean insertWorkout(String exersise, int repetitions , int weight, String date) {
-        int userID = Session.getUserID();
+    /**
+     * Gets all workouts of logged in user of certain exercise
+     * @param exercise What workouts will be loaded
+     * @return all workouts of certain exercise
+     */
+    public ArrayList<Workout> getWorkoutsByExerciseName(String exercise) {
+        ArrayList<Workout> workouts = new ArrayList<>();
+        String query = "select * from " + ResourceTables.WORKOUTS +
+                " where userID = ? and exercise = ?";
+        try (PreparedStatement preparedStatement = connection.prepareStatement(query)) {
+            preparedStatement.setInt(1, userID);
+            preparedStatement.setString(2, exercise);
+            ResultSet resultSet = preparedStatement.executeQuery();
+            while (resultSet.next()) {
+                Workout workout = loadWorkoutFromResultSet(resultSet);
+                if (workout != null) workouts.add(workout);
+            }
+            return workouts;
+        } catch (SQLException e) {
+            e.printStackTrace();
+            return null;
+        }
+
+    }
+
+    public boolean insertWorkout(Workout workout) {
         String query = "insert into  " + ResourceTables.WORKOUTS + "(exercise, repetitions, weight , date, userId) values (?,?,?,?,?)";
-        try(PreparedStatement preparedStatement = connection.prepareStatement(query)) {
-            preparedStatement.setString(1, exersise);
-            preparedStatement.setInt(2, repetitions);
-            preparedStatement.setInt(3, weight);
-            preparedStatement.setString(4, date);
-            preparedStatement.setInt(5,userID);
+        try (PreparedStatement preparedStatement = connection.prepareStatement(query)) {
+            preparedStatement.setString(1, workout.getExercise());
+            preparedStatement.setInt(2, workout.getRepetitions());
+            preparedStatement.setString(3, workout.getWeight());
+            preparedStatement.setString(4, workout.getDate());
+            preparedStatement.setInt(5, userID);
             return preparedStatement.executeUpdate() > 0;
         } catch (SQLException e) {
             e.printStackTrace();
