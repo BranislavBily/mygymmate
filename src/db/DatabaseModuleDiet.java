0dc133cec9d2f11b5282cd1149295995155ce3bd
Branislav Bily <branislav.bily@gmail.com>
2019-05-06 21:34:47 +0200

    REF I pressed alt + ctrl + l

--- src/db/DatabaseModuleDiet.java
+++ src/db/DatabaseModuleDiet.java
@@ -20,31 +20,28 @@ public class DatabaseModuleDiet {
 
     public DatabaseModuleDiet() {
         connection = SqliteConnection.connector();
-        if(connection == null) System.exit(1);
+        if (connection == null) System.exit(1);
     }
 
     public Diet loadDiet(int userID) {
         Diet diet = new Diet();
         ResultSet resultSet;
         String query = "select * from " + ResourceTables.DIET + " where userID = ?";
-        try(PreparedStatement preparedStatement = connection.prepareStatement(query)) {
+        try (PreparedStatement preparedStatement = connection.prepareStatement(query)) {
             preparedStatement.setInt(1, userID);
             resultSet = preparedStatement.executeQuery();
-
-
-                if(loadDietFromResultSet(resultSet) != null) {
-                    diet=loadDietFromResultSet(resultSet);
-
-                }
-
+            if (loadDietFromResultSet(resultSet) != null) {
+                diet = loadDietFromResultSet(resultSet);
+            }
             return diet;
         } catch (SQLException e) {
             e.printStackTrace();
             return null;
         }
     }
+
     private Diet loadDietFromResultSet(ResultSet resultSet) {
-        Diet diet=new Diet();
+        Diet diet = new Diet();
         try {
             diet.setActualCalories(resultSet.getInt("calories"));
             diet.setActualProtein(resultSet.getInt("protein"));
@@ -60,7 +57,7 @@ public class DatabaseModuleDiet {
     public boolean updateCalories(int calories) {
         int userID = Session.getUserID();
         String query = "update " + ResourceTables.DIET + " set calories = ? where userId = ?";
-        try(PreparedStatement preparedStatement = connection.prepareStatement(query)) {
+        try (PreparedStatement preparedStatement = connection.prepareStatement(query)) {
             preparedStatement.setInt(1, calories);
             preparedStatement.setInt(2, userID);
             return preparedStatement.executeUpdate() > 0;
@@ -69,10 +66,11 @@ public class DatabaseModuleDiet {
         }
         return false;
     }
+
     public boolean updateProtein(int protein) {
         int userID = Session.getUserID();
         String query = "update " + ResourceTables.DIET + " set protein= ? where userId = ?";
-        try(PreparedStatement preparedStatement = connection.prepareStatement(query)) {
+        try (PreparedStatement preparedStatement = connection.prepareStatement(query)) {
             preparedStatement.setInt(1, protein);
             preparedStatement.setInt(2, userID);
             return preparedStatement.executeUpdate() > 0;
@@ -81,10 +79,11 @@ public class DatabaseModuleDiet {
         }
         return false;
     }
+
     public boolean updateWater(int water) {
         int userID = Session.getUserID();
         String query = "update " + ResourceTables.DIET + " set water = ? where userId = ?";
-        try(PreparedStatement preparedStatement = connection.prepareStatement(query)) {
+        try (PreparedStatement preparedStatement = connection.prepareStatement(query)) {
             preparedStatement.setInt(1, water);
             preparedStatement.setInt(2, userID);
             return preparedStatement.executeUpdate() > 0;
@@ -93,7 +92,6 @@ public class DatabaseModuleDiet {
         }
         return false;
     }
-
 
 
 }
