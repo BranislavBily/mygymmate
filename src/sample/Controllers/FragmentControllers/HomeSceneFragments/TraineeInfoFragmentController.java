3ffa58fbe6a166d9c9e34617cc9e8597ae2854b9
Branislav Bily <branislav.bily@gmail.com>
2019-05-11 12:01:34 +0200

    FEAT Delete Trainee and Add Trainee

--- src/sample/Controllers/FragmentControllers/HomeSceneFragments/TraineeInfoFragmentController.java
+++ src/sample/Controllers/FragmentControllers/HomeSceneFragments/TraineeInfoFragmentController.java
@@ -8,17 +8,18 @@ import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.TableColumn;
-import javafx.scene.control.TableRow;
-import javafx.scene.control.TableView;
+import javafx.scene.control.*;
 import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.stage.Modality;
 import javafx.stage.Stage;
+import sample.Controllers.PopUpWindowControllers.AddTraineeController;
 import sample.Controllers.PopUpWindowControllers.TraineeInfoController;
+import sample.Dialogs.DeleteTraineeDialog;
 import sample.Resources.ResourceFXML;
 
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Optional;
 
 public class TraineeInfoFragmentController {
 
@@ -77,5 +78,52 @@ public class TraineeInfoFragmentController {
         } catch (IOException e) {
             e.printStackTrace();
         }
+    }
+
+    @FXML
+    private void onButtonAddTraineePressed() {
+        Stage stage =  new Stage();
+        FXMLLoader loader = new FXMLLoader(getClass().getResource(ResourceFXML.ADD_TRAINEE));
+        try {
+            Parent root = loader.load();
+            AddTraineeController addTraineeController = loader.getController();
+            addTraineeController.onCreate();
+            Scene scene = new Scene(root);
+            stage.setScene(scene);
+            stage.setTitle("TraineeInfo");
+            stage.setResizable(false);
+            stage.initModality(Modality.APPLICATION_MODAL);
+            stage.showAndWait();
+            loadTraineesIntoTable();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @FXML
+    private void onMenuItemDeletePressed() {
+        ProfileData profileData = tableViewTrainees.getSelectionModel().getSelectedItem();
+        if(profileData == null) {
+            System.out.println("Prazdny klik");
+        } else {
+            boolean deleteTrainee = getDeleteTraineeDialogAnswer(profileData);
+            if(deleteTrainee) {
+                if(databaseModuleInfo.deleteTraineeFromTrainer(profileData.getUsername())) {
+                    loadTraineesIntoTable();
+                    System.out.println("Success");
+                } else {
+                    System.out.println("Error while deleting Trainee from Trainer");
+                }
+            } else {
+                System.out.println("Vypol sa dialog");
+            }
+        }
+
+    }
+
+    private boolean getDeleteTraineeDialogAnswer(ProfileData profileData) {
+        DeleteTraineeDialog deleteTraineeDialog = new DeleteTraineeDialog(Alert.AlertType.CONFIRMATION, profileData);
+        Optional<ButtonType> result = deleteTraineeDialog.showAndWait();
+        return result.get() == ButtonType.OK;
     }
 }
