57158bb9565edbc9d1e8819b3ff938e6ea636fa0
Branislav Bily <branislav.bily@gmail.com>
2019-05-13 18:27:17 +0200

    FEAT Add weight now sorts to the newest one

--- src/db/DatabaseModuleWeight.java
+++ src/db/DatabaseModuleWeight.java
@@ -9,7 +9,10 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.text.ParseException;
 import java.text.SimpleDateFormat;
+import java.time.LocalDate;
+import java.time.ZoneId;
 import java.util.Date;
 
 public class DatabaseModuleWeight {
@@ -28,7 +31,7 @@ public class DatabaseModuleWeight {
         try(PreparedStatement preparedStatement = connection.prepareStatement(query)) {
             preparedStatement.setInt(1,id);
             preparedStatement.setDouble(2,weight);
-            preparedStatement.setString(3,new SimpleDateFormat("dd.MM.yyyy").format(new Date()));
+            preparedStatement.setString(3,new SimpleDateFormat("yyyy-MM-dd HH:mm:ss.SSS").format(new Date()));
             return preparedStatement.executeUpdate() > 0;
         } catch (SQLException e) {
             e.printStackTrace();
@@ -36,15 +39,27 @@ public class DatabaseModuleWeight {
         }
     }
 
+    private String formatDate(String date) {
+        SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss.SSS");
+        try {
+            Date dateD = format.parse(date);
+            LocalDate localDate = dateD.toInstant().atZone(ZoneId.systemDefault()).toLocalDate();
+            return localDate.getDayOfMonth() + "." + (localDate.getMonthValue()) + "." + (localDate.getYear());
+        } catch (ParseException e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+
     public Weight getUserWeight() {
-        String query = "select weight, date from " + ResourceTables.WEIGHT + " where userID = ?  order By date desc LIMIT 1";
+        String query = "select weight, date from " + ResourceTables.WEIGHT + " where userID = ?  order By datetime(date) desc LIMIT 1";
         Weight weight=new Weight();
         try(PreparedStatement preparedStatement = connection.prepareStatement(query)) {
             preparedStatement.setInt(1, userID);
             ResultSet resultSet = preparedStatement.executeQuery();
             if(resultSet.next()) {
                 weight.setWeight(resultSet.getDouble("weight"));
-                weight.setDate(resultSet.getString("date"));
+                weight.setDate(formatDate(resultSet.getString("date")));
                 return weight;
             }
             return null;
