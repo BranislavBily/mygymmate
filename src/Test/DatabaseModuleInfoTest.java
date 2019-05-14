c2d6fb06286084c40f2a30aff1db90cb208e077b
Branislav Bily <branislav.bily@gmail.com>
2019-05-14 22:13:41 +0200

    FEAT JUNIT

---
+++ src/Test/DatabaseModuleInfoTest.java
@@ -0,0 +1,40 @@
+package Test;
+
+class DatabaseModuleInfoTest {
+
+    @org.junit.jupiter.api.BeforeEach
+    void setUp() {
+    }
+
+    @org.junit.jupiter.api.AfterEach
+    void tearDown() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void getTraineesOfLoggedTrainer() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void getMyTrainer() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void getMyTrainerID() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void getAllTraineesByName() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void getAllTraineesByName1() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void addTraineeToTrainer() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void deleteTraineeFromTrainer() {
+    }
+}
